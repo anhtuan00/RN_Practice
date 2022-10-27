67532276e557fca4fea679b340f8b7914887df23
anhtuan00 <67965462+anhtuan00@users.noreply.github.com>
2022-10-27 21:53:13 +0700

    newfeed and zalo

---
+++ Homework/newfeed.png/DemoProject/App.js
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import NewFeed from './screens/NewFeed';
+import Zalo from './screens/Zalo';
+
+export default function App() {
+  return <NewFeed />;
+  // return <Zalo />;
+}
